e88cd4651cbe84f6762dee9b0314ac1e4823359c
12944qwerty <qwerty12944qwerty@gmail.com>
2020-08-27 16:22:09 -0500

    property functions instead

--- color_palette/color.py
+++ color_palette/color.py
@@ -6,37 +6,39 @@ class Color:
         if self.mode is None or (self.mode == 'rgb' and len(value) != 3) or (self.mode == 'hex' and len(value) != 6):
             errors.raiseColorValueError(value)
         self.value = value
-        self.red = None
-        self.green = None
-        self.blue = None
 
-        if self.mode == "rgb":
-            self.red = self.value[0]
-            self.green = self.value[1]
-            self.blue = self.value[2]
-
-        elif self.mode == "hex":
-            self.red = self.value[0:2]
-            self.green = self.value[2:4]
-            self.blue = self.value[4:6]
-
-        else:
+        if not self.mode in ("rgb","hex"):
             errors.raiseColorModeError(self.mode)
+
+    @property
+    def red(self):
+        if self.mode == "rgb":
+            return self.value[0]
+        elif self.mode == "hex":
+            return self.value[0:2]
+
+    @property
+    def blue(self):
+        if self.mode == "rgb":
+            return self.value[1]
+        elif self.mode == "hex":
+            return self.value[2:4]
+
+    @property
+    def green(self):
+        if self.mode == "rgb":
+            return self.value[2]
+        elif self.mode == "hex":
+            return self.value[4:6]
 
     def switch(self, mode: str):
         if mode != self.mode:
             if mode == "rgb":
                 self.value = conversion.hex_rgb(self.value)
-                self.red = self.value[0]
-                self.green = self.value[1]
-                self.blue = self.value[2]
                 self.mode = mode
 
             elif mode == "hex":
                 self.value = conversion.rgb_hex(self.value)
-                self.red = self.value[0:2]
-                self.green = self.value[2:4]
-                self.blue = self.value[4:6]
                 self.mode = mode
 
             else:
