c64f4709a65ba9186fee39519ba53604549b6072
SnowballSH <derek070211@gmail.com>
2020-08-27 11:02:40 -0500

    >=3.6

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup
 
 requirements = []
 with open('requirements.txt') as f:
-  requirements = f.read().splitlines()
+    requirements = f.read().splitlines()
 
 with open("README.md", "r") as f:
- 	readme = f.read()
-	
+    readme = f.read()
+
 setup(name="color_pallete",
-	author="SnowballSH, one-wq, 12944qwerty",
-	version="0.0",
-	description="A color palette module",
-	long_description=readme,
-	long_description_content_type="text/md",
-	install_requires=requirements,
-	python_requires=">=3.5"
-)
+      author="SnowballSH, one-wq, 12944qwerty",
+      version="0.0",
+      description="A color palette module",
+      long_description=readme,
+      long_description_content_type="text/md",
+      install_requires=requirements,
+      python_requires=">=3.6"
+      )
