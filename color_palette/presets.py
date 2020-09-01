6bc12aac545b195c9218870f9e86bfd93b70a14b
SnowballSH <derek070211@gmail.com>
2020-08-31 20:21:02 -0500

    more colors in presets!

--- color_palette/presets.py
+++ color_palette/presets.py
@@ -2,9 +2,35 @@ from . import color
 
 Color = color.Color
 
+# Basic colors
+
+black = Color((0, 0, 0))
+white = Color((255, 255, 255))
+
+silver = Color((192, 192, 192))
+grey = gray = Color((128, 128, 128))
+
 red = Color((255, 0, 0))
 green = Color((0, 255, 0))
 blue = Color((0, 0, 255))
+
+dark_red = Color((139, 0, 0))
+dark_green = Color((0, 139, 0))
+dark_blue = Color((0, 0, 149))
+
 yellow = Color((255, 255, 0))
-magenta = Color((255, 0, 255))
-cyan = Color((0, 255, 255))
+magenta = fuchsia = Color((255, 0, 255))
+cyan = aqua = Color((0, 255, 255))
+
+orange = Color((255, 165, 0))
+lime = Color((173, 255, 47))
+turquoise = Color((64, 224, 208))
+
+# More colors
+
+maroon = Color((128, 0, 0))
+olive = Color((128, 128, 0))
+olive_green = Color((0, 128, 0))
+purple = Color((128, 0, 128))
+teal = Color((0, 128, 128))
+navy = Color((0, 0, 128))
